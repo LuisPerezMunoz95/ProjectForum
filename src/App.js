28fef928662c9869b468a060342f999e34decbd4
LuisPerezMunoz95 <luis.perezmunoz95@gmail.com>
2022-12-02 14:38:55 +0100

    Funciona el post y el get en el front

--- src/App.js
+++ src/App.js
@@ -12,24 +12,11 @@ import PostBody from "./component/PostBody/PostBody";
 
 function App() {
   const urlForum = "https://localhost:7261/forum";
-  const [threads, setThreads] = useState([
-    {
-      "title": "Title 1",
-      "text": "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
-      "Category": "Question",
-    },
-    {
-      "title": "Title 1",
-      "text": "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
-      "Category": "Suggestion",
-    },
-  ]);
- 
-  
+  const [threads, setThreads] = useState([]);
   const [post, setPost] = useState({
     "title": "",
     "text": "",
-    "Category": "Question",
+    "category": "Question",
   });
 
   const [newThread, setNewThread] = useState(false);
@@ -51,12 +38,11 @@ function App() {
   }
   function addNewPost() {
     // alert("Add new post to API");
-    console.log(post)
     axios.post(urlForum, {
       title: post.title,
       text: post.text,
       category: post.category,
-    }).then((response) => console.log(response));
+    });
     threads.push(post);
     setThreads(threads);
     setNewThread(false);
